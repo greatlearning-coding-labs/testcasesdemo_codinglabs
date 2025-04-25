2c28eadd1964f5a23f52012a972f9b1777de0ed9
sakshi-mohinia <164994733+sakshi-mohinia@users.noreply.github.com>
2025-04-25 13:18:28 +0530

    Update AssignmentTest.java

--- TestCaseValidation/src/test/java/AssignmentTest.java
+++ TestCaseValidation/src/test/java/AssignmentTest.java
@@ -8,28 +8,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AssignmentTest {
 
-	@Test
+
 	void testNormalSentence() {
-		assertEquals("dlroW olleH", Assignment.reverseWords("Hello World"));
+		
 	}
 
-	@Test
+
 	void testSingleWord() {
-		assertEquals("avaJ", Assignment.reverseWords("Java"));
+		
 	}
 
-	@Test
+	
 	void testEmptyString() {
-		assertEquals("", Assignment.reverseWords(""));
+		
 	}
 
-	@Test
+	
 	void testNullInput() {
-		assertNull(null,Assignment.reverseWords(""));
+		
 	}
 
-	@Test
+	
 	void testMultipleSpaces() {
-		assertEquals("dlroW   olleH", Assignment.reverseWords("Hello   World"));
+	
 	}
 }
